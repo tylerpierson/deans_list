553ab1c439950d9a14802b471836adf11c1edc32
Tyler Pierson <tylerpierson92@gmail.com>
2024-04-07 14:43:06 -0500

    Created the userSchema

--- models/user.js
+++ models/user.js
@@ -2,8 +2,23 @@ const { model, Schema } = require('mongoose')
 
 
 const userSchema = new Schema ({
-    title: { required: true, type: String },
-    completed: { required: true, type: Boolean }
+    firstName: { required: true, type: String },
+    lastName: { required: true, type: String },
+    email: { required: true, type: String, unique: true },
+    password: { type: String, required: true },
+    campusNum: String,
+    campus: String,
+    role: {
+        type: String,
+        enum: ['admin', 'teacher', 'student', 'parent'],
+        default: 'admin'
+    },
+    subjects: [String],
+    students: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    studentId: String,
+    teachers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    gradeLevel: { type: String, required: true },
+    assignments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Assignment'}],
 }, {
     timestamps: true
 })
